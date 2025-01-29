0d5a3c68af2daa13dfe9828ce31e999af5bd8c07
ArsenShakirov <shakirov.arsen05@gmail.com>
2025-01-29 23:20:16 +0500

    Create Syntax.py

---
+++ Syntax.py
@@ -0,0 +1,14 @@
+if 5 > 2:
+  print("Five is greater than two!")  
+#Five is greater than two!
+
+if 5 > 2:
+print("Five is greater than two!")
+
+#Syntax error due to no space
+
+if 5 > 2:
+ print("Five is greater than two!")
+        print("Five is greater than two!")
+
+#Syntax error due to different number of spaces in same block of code
