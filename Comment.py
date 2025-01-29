b5e149d969df9b2d4785159b377229288b130bab
ArsenShakirov <shakirov.arsen05@gmail.com>
2025-01-29 23:23:05 +0500

    Create Comment.py

---
+++ Comment.py
@@ -0,0 +1,20 @@
+#This is a comment
+print("Hello, World!")
+
+#Comments starts with a #, and Python will ignore them:
+
+#This is how you do multiple comments:
+
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!") #Hello, World!
+
+or
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")  #Hello, World!
