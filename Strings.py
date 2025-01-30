86ed433aeeb3dca6d61880150d7833326949d890
ArsenShakirov <shakirov.arsen05@gmail.com>
2025-01-30 11:58:34 +0500

    Create Strings.py

---
+++ Strings.py
@@ -0,0 +1,42 @@
+print("Hello")  #Hello
+print('Hello')  #Hello
+
+print("It's alright")    #It's alright
+print("He is called 'Johnny'")  #He is called 'Johnny'
+print('He is called "Johnny"')  #He is called "Johnny"
+
+a = "Hello"
+print(a)  #Hello
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)  #the multiline text
+
+a = "Hello, World!"
+print(a[1])  #e
+
+for x in "banana":
+  print(x)   #type banana letter by letter in column
+
+a = "Hello, World!"
+print(len(a))  #13
+
+txt = "The best things in life are free!"
+print("free" in txt)  #True
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")  #Yes, 'free' is present.
+
+txt = "The best things in life are free!"
+print("expensive" not in txt)  #True
+
+b = "Hello, World!"
+print(b[2:5])  #llo
+
+b = "Hello, World!"
+print(b[:5])  #Hello
+
+
