c019a5a5a98004ec2a870938cfbe136bc6148c96
ArsenShakirov <shakirov.arsen05@gmail.com>
2025-02-01 23:02:07 +0500

    Create class3.py

---
+++ class3.py
@@ -0,0 +1,20 @@
+class Shape:
+    def __init__(self):
+        pass
+
+    def area(self):
+        return 0
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+shape = Shape()
+print("Shape")
+
+rectangle = Rectangle(4, 5)
+print("Rectangle")
