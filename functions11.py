5c62c685e99dbd50924d6587b10bd7f64498b0f4
ArsenShakirov <shakirov.arsen05@gmail.com>
2025-02-06 12:50:11 +0500

    Update functions11.py

--- functions11.py
+++ functions11.py
@@ -1,6 +1,10 @@
 def is_palindrome(s):
-    s = ''.join(e for e in s if e.isalnum()).lower()
+    c = []
+    for e in s:
+        if e.isalnum():
+            c.append(e)
+    s = ''.join(c).lower()
     return s == s[::-1]
 
-word_or_phrase = input("Enter a word or phrase: ")
-print(is_palindrome(word_or_phrase))
+w = input("Enter a word")
+print(is_palindrome(w))
