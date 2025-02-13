52483c75931cbe9742ee3250edf3d6c906e15687
ArsenShakirov <shakirov.arsen05@gmail.com>
2025-02-13 12:13:19 +0500

    Update 3.py

--- generators/3.py
+++ generators/3.py
@@ -1,9 +1,9 @@
-def divisible_by_3_and_4(n):
+def divisible(n):
     for i in range(n + 1):
         if i % 3 == 0 and i % 4 == 0:
             yield i
 
 n = int(input("Enter a number: "))
 
-for num in divisible_by_3_and_4(n):
+for num in divisible(n):
     print(num)
