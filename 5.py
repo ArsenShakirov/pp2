1eff7f59597107b7a57d3d9af154b446bc40647f
ArsenShakirov <shakirov.arsen05@gmail.com>
2025-03-05 21:49:06 +0500

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,2 +1,7 @@
-tp = tuple([True, False, True, True])
-print((all(tp)))
+def all_true(t):
+    return all(t)
+t1 = (True, True, True)
+t2 = (True, False, True)
+
+print(all_true(t1))  
+print(all_true(t2)) 
