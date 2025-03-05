53a8df0058ca36211285088f52078de5f989fd1c
ArsenShakirov <shakirov.arsen05@gmail.com>
2025-03-05 21:45:47 +0500

    Create 8.py

---
+++ dir-and-files/8.py
@@ -0,0 +1,9 @@
+import os
+
+def delete(path):
+    if os.path.exists(path):
+        if os.access(path, os.W_OK):
+            os.remove(path)
+            print(f"File '{path}' deleted.")
+        else:
+            print(f"Cannot delete '{path}'.")
